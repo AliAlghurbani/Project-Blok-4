0564303285cbed53cc60efdb18522fe4f69e099f
Ali <alimohalghurbani@gmail.com>
2023-06-07 10:28:44 +0200

    commit 1

---
+++ site/database.php
@@ -0,0 +1,11 @@
+<?php
+
+$host  = "mariadb";
+$dbuser = "root";
+$dbpass = "password";
+$dbname = "projectblok4";
+
+// Maak een  database connectie
+$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+
+?>
