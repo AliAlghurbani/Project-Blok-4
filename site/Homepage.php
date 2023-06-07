f643183a81963fa0a75a8355171bcb89f7bada57
Ali <alimohalghurbani@gmail.com>
2023-06-07 13:21:14 +0200

    commit 2

--- site/Homepage.php
+++ site/Homepage.php
@@ -1,25 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/icon.png">
     <link rel="stylesheet" href="css/style.css">
-    <title> </title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap" rel="stylesheet">
+    <title> Locasion </title>
 </head>
+
 <body>
 
-<header>
-    <div class="logo">
+    <div class="shadow">
+        <header>
 
+            <div class="logo">
+                <img src="images/Logo1-removebg-preview.png" alt="LOGO" class="logoImg">
+            </div>
+            <nav>
+                <ul class="ulnav">
+                    <li class="linav"> <a href="#"> Explore </a></li>
+                    <li class="linav"> <a href="#"> Apps </a></li>
+                    <li class="linav"><a href="#"> More </a></li>
+                    <li class="linav"><a href="#"> Contact </a></li>
+                </ul>
+            </nav>
+
+            <div class="loginHeader">
+                <a href="#" class="inloggenButton"> INLOGGEN </a>
+            </div>
+        </header>
     </div>
-    <nav>
 
-    </nav>
-    <div class="loginHeader">
+    <main>
 
-    </div>
-</header>
-    
+    </main>
+
+    <footer>
+
+        <ul class="ulfooter">
+            <li class="lifooter"> <a href="#"> ABOUT </a></li>
+            <li> | </li>
+            <li class="lifooter"> <a href="#"> CODE </a></li>
+            <li> | </li>
+            <li class="lifooter"> <a href="#"> CONTACT </a></li>
+            <li> | </li>
+            <li class="lifooter"> <a href="#"> PRIVACY </a></li>
+            <li> | </li>
+            <li class="lifooter"> <a href="#"> SETUP </a></li>
+            <li> | </li>
+            <li class="lifooter"> <a href="#"> SITEMAP </a></li>
+        </ul>
+
+        <div class="divsocials">
+            <ul class="ulsocials">
+                <li class="lisocials"><a href="#" class="fa fa-facebook"></a></li>
+                <li class="lisocials"><a href="#" class="fa fa-youtube"></a></li>
+                <li class="lisocials"><a href="#" class="fa fa-twitter"></a></li>
+                <li class="lisocials"><a href="#" class="fa fa-instagram"></a></li>
+            </ul>
+        </div>
+
+        <div class="footerdetails">
+            <h1> ABOUT . PRIVACY POLICY </h1>
+            <p> Copyright © 2023 Static Media®/ Locasion.com / All Rights Reserved </p>
+        </div>
+
+    </footer>
+
 </body>
+
 </html>
