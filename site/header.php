ba4c2cf84d9dd4c21625175650f4ab80c1698f33
Ali <alimohalghurbani@gmail.com>
2023-06-08 09:29:57 +0200

    commit 3

---
+++ site/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/icon.png">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap" rel="stylesheet">
+    <title> Locasion </title>
+</head>
+
+<body>
+
+    <div class="shadow">
+        <header>
+
+            <div class="logo">
+                <img src="images/Logo1-removebg-preview.png" alt="LOGO" class="logoImg">
+            </div>
+            <nav>
+                <ul class="ulnav">
+                    <li class="linav"> <a href="#"> Explore </a></li>
+                    <li class="linav"> <a href="#"> Apps </a></li>
+                    <li class="linav"><a href="#"> More </a></li>
+                    <li class="linav"><a href="#"> Contact </a></li>
+                </ul>
+            </nav>
+
+            <div class="loginHeader">
+                <a href="#" class="signInBtn"> Sign in </a>
+                <a href="#" class="regisBtn"> Register </a>
+            </div>
+        </header>
+    </div>
